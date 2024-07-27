947f9b41adb43bc13d284804689d07fa8a8b4407
SameerRanjanMansingh <sameerranjan0367@gmail.com>
2024-07-27 14:54:03 +0530

    dags-hub deployment changes

--- src/evaluation.py
+++ src/evaluation.py
@@ -3,8 +3,14 @@ import pandas as pd
 import joblib
 from sklearn.metrics import precision_score, recall_score, f1_score
 import json
+import mlflow
 
 
+import dagshub
+dagshub.init(repo_owner='SameerRanjanMansingh', repo_name='DSMP_fun', mlflow=True)
+
+
+mlflow.set_tracking_uri("https://dagshub.com/SameerRanjanMansingh/DSMP_fun.mlflow")
 
 def evaluate_model(cv_results: np.ndarray) -> dict:
     metrics = {
@@ -27,24 +33,36 @@ def evaluate_additional_metrics(model, X_test: pd.DataFrame, y_test: pd.Series) 
 if __name__ == "__main__":
     try:
         cv_results = np.load("models/cv_results.npy")
-        basic_metrics = evaluate_model(cv_results)
-        print(f"Cross-validation accuracy: {basic_metrics['mean_score']} +/- {basic_metrics['std_dev']} (std) min: {basic_metrics['min_score']}, max: {basic_metrics['max_score']}")
 
         X = pd.read_pickle("data/processed/X.pkl")
         y = pd.read_pickle("data/processed/y.pkl")
+
         model_pipe = joblib.load("models/model_pipe.pkl")
         
         from sklearn.model_selection import train_test_split
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
         
         model_pipe.fit(X_train, y_train)
-        additional_metrics = evaluate_additional_metrics(model_pipe, X_test, y_test)
-
-        metrics = {**basic_metrics, **additional_metrics}
-        print(f"Precision: {metrics['precision']}, Recall: {metrics['recall']}, F1 Score: {metrics['f1']}")
-
-        with open("results/metrics.json", "w") as f:
-            json.dump(metrics, f)
+        
+        mlflow.set_experiment('iris_dt')
+        with mlflow.start_run(run_name='run1'):
+            mlflow.log_param("n_estimators", 160)
+            mlflow.log_param("max_features", 0.4)
+            mlflow.log_param("min_samples_split", 2)
+            mlflow.log_param("n_jobs", -1)
+            mlflow.log_param("random_state", 0)
+            
+            # Log metrics
+            metrics = evaluate_model(cv_results)
+            for key, value in metrics.items():
+                mlflow.log_metric(key, value)
+            
+            additional_metrics = evaluate_additional_metrics(model_pipe, X_test, y_test)
+            for key, value in additional_metrics.items():
+                mlflow.log_metric(key, value)
+            
+            # Log the model
+            mlflow.sklearn.log_model(model_pipe, "model")
     except FileNotFoundError as e:
         print(f"Error: {e}")
         raise
